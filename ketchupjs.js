ed8b5f93eb3fe2625c715c00b1e549ed2dfa23d2
kevin tauzer <kevint@kevins-MBP.home>
2019-03-12 16:10:28 -1000

    post to lambda function with ketchupjs library

---
+++ ketchupjs.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+let configObj = { key: '' };
+
+function config(key) {
+  configObj.key = key;
+}
+
+function save(metric, value, user_id) {
+  const body = {
+    key: configObj.key,
+    metric: metric,
+    value: value.toString(),
+    user_id: user_id.toString()
+  };
+
+  fetch('https://d3zevizvrk.execute-api.us-west-2.amazonaws.com/dev/queue', {
+    method: 'post',
+    body: JSON.stringify(body),
+    header: { 'Content-Type': 'application/json' }
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => { 
+      console.log(err)
+    })
+}
+
+module.exports = {
+  config,
+  save
+}
+
+
