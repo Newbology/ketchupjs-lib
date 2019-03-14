2ae43280fd6e2cdbedaad190330adb608ea08b21
kevin tauzer <kevint@kevins-MBP.home>
2019-03-13 15:52:46 -1000

    create most function edit configObj

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const fetch = require('node-fetch');
 
-let configObj = { key: '' };
+let configObj = {};
 
-function config(key) {
+function config(name, key) {
+  configObj.name = name;
   configObj.key = key;
-}
+};
 
-function save(metric, value, user_id) {
-  if(!user_id){
-    user_id = 0
-  }
+function save(metric, value, user_name) {
   const body = {
+    name: configObj.name,
     key: configObj.key,
     metric: metric,
     value: value.toString(),
-    user_id: user_id.toString()
+    user_name: user_name.toString()
   };
 
   fetch('https://rdz8bth03m.execute-api.us-west-2.amazonaws.com/dev/queue', {
@@ -24,14 +23,35 @@ function save(metric, value, user_id) {
   })
     .then(res => res.json())
     .then(json => console.log(json))
-    .catch(err => { 
-      console.log(err)
-    })
-}
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+function most(metric, value, user_name) {
+  const body = {
+    name: configObj.name,
+    key: configObj.key,
+    metric: metric,
+    value: value.toString(),
+    user_name: user_name.toString(),
+    most: true
+  };
+
+  fetch('https://rdz8bth03m.execute-api.us-west-2.amazonaws.com/dev/queue', {
+    method: 'post',
+    body: JSON.stringify(body),
+    header: { 'Content-Type': 'application/json' }
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   config,
-  save
-}
-
-
+  save,
+  most
+};
