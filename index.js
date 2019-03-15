3975b42bde16e285e331c6593090c88a35bb5aae
kevin tauzer <kevint@kevins-MBP.home>
2019-03-14 16:54:21 -1000

    create checks for bad input, remove lambda from library

--- index.js
+++ index.js
@@ -1,73 +1,78 @@
 const fetch = require('node-fetch');
-
+//in development put your api gateway here
+const lambda = '';
 let configObj = {};
 
-function config(name, key) {
-  configObj.name = name;
+function config(key) {
   configObj.key = key;
 }
 
 function save(metric, value, user_name) {
-  if( typeof metric !== "string"){
-    throw `Metric needs to be a String, Input receieved was a ${typeof metric}`
+  if (typeof metric !== 'string') {
+    throw `Metric needs to be a String, Input receieved was a ${typeof metric}`;
   }
   if (!user_name) {
     user_name = 'default';
-  };
-  if(typeof value !== "number"){
+  }
+  if (typeof value !== 'number') {
     throw `Value needs to be a Number, Input receieved was a ${typeof value}`;
   }
-  if( typeof user_name !== "string"){
-    throw `user_name needs to be a String, Input receieved was a ${typeof user_name}`
+  if (typeof user_name !== 'string') {
+    throw `user_name needs to be a String, Input receieved was a ${typeof user_name}`;
   }
-  
+
   const body = {
-    name: configObj.name,
     key: configObj.key,
     metric: metric,
     value: value.toString(),
     user_name: user_name
   };
 
-  fetch('https://6f2sov5oq0.execute-api.us-west-2.amazonaws.com/dev/queue', {
+  fetch(lambda, {
     method: 'post',
     body: JSON.stringify(body),
     header: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json())
+    .then(response => {
+      if (response.status !== 200) {
+        throw response.statusText;
+      }
+    })
     .catch(err => {
       throw err;
     });
 }
 
 function most(metric, value, user_name) {
-  if( typeof metric !== "string"){
-    throw `Metric needs to be a String, Input receieved was a ${typeof metric}`
+  if (typeof metric !== 'string') {
+    throw `Metric needs to be a String, Input receieved was a ${typeof metric}`;
   }
   if (!user_name) {
     user_name = 'default';
-  };
-  if(typeof value !== "number"){
+  }
+  if (typeof value !== 'number') {
     throw `Value needs to be a Number, Input receieved was a ${typeof value}`;
   }
-  if( typeof user_name !== "string"){
-    throw `user_name needs to be a String, Input receieved was a ${typeof user_name}`
+  if (typeof user_name !== 'string') {
+    throw `user_name needs to be a String, Input receieved was a ${typeof user_name}`;
   }
   const body = {
-    name: configObj.name,
     key: configObj.key,
     metric: metric,
     value: value.toString(),
     user_name: user_name,
     most: true
   };
-
-  fetch('https://6f2sov5oq0.execute-api.us-west-2.amazonaws.com/dev/queue', {
+  fetch(lambda, {
     method: 'post',
     body: JSON.stringify(body),
     header: { 'Content-Type': 'application/json' }
   })
-    .then(response => response.json())
+    .then(response => {
+      if (response.status !== 200) {
+        throw response.statusText;
+      }
+    })
     .catch(err => {
       throw err;
     });
